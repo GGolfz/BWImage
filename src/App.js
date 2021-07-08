305ceec2df24143bddeb8ebebaae799dbc544150
GGolfz <wisarutgolf11@gmail.com>
2021-07-08 19:43:18 +0700

    format code :sparkles:

--- src/App.js
+++ src/App.js
@@ -26,25 +26,26 @@ function App() {
     console.log(dataText);
     setData(JSON.parse(dataText));
   };
-  const handleEditSpecific = (i,j) =>{
+  const handleEditSpecific = (i, j) => {
     const val = prompt("Enter your value");
     try {
       let parsedValue = parseFloat(val).toFixed(2);
-      if(parsedValue < 0 || parsedValue > 1) throw new Error("Value is out of bound");
-      let temp =[...data]
-      temp[i][j] = parsedValue
-      setData(temp)
-      setDisplay([...temp])
-    } catch(err){
-      console.log(err)
+      if (parsedValue < 0 || parsedValue > 1)
+        throw new Error("Value is out of bound");
+      let temp = [...data];
+      temp[i][j] = parsedValue;
+      setData(temp);
+      setDisplay([...temp]);
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
   const handleCopy = () => {
     var copyText = document.getElementById("dataImage");
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
-  }
+  };
   const renderImage = () => {
     return (
       <div style={{ display: "flex", flexFlow: "column" }}>
@@ -60,7 +61,7 @@ function App() {
                   width: "24px",
                   height: "24px",
                 }}
-                onClick={()=>handleEditSpecific(i,j)}
+                onClick={() => handleEditSpecific(i, j)}
               ></div>
             ))}
           </div>
@@ -78,7 +79,7 @@ function App() {
         alignItems: "center",
       }}
     >
-      <h1 style={{margin:'.25rem'}}>BWImage</h1>
+      <h1 style={{ margin: ".25rem" }}>BWImage</h1>
       <textarea
         onChange={handleChangeDisplay}
         id="dataImage"
